809e6a32f66b89bd83b60737aee4990fe72d398f
Axekill <andrei3.ru@ya.ru>
2023-08-21 13:55:08 +0300

    разделил строки 2 методами

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "привет,мир,как дела?";
+        String[] s1 = s.split(",", 3);
+        for (String str : s1) {
+            System.out.println(str);
+        }
+
+        System.out.println("*********************************************");
+
+        Stream<String> stream = Arrays.stream(s.split(",",3));
+        stream.forEachOrdered(System.out::println);
+    }
+
+
+}
